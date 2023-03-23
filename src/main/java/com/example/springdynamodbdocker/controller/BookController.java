2d10edd8e80504820d29e416de51bc4927f6db5f
tihomir.valchanov <tihomir.valchanov@accedia.com>
2023-03-23 11:04:16 +0200

    Add delete endpoint

--- src/main/java/com/example/springdynamodbdocker/controller/BookController.java
+++ src/main/java/com/example/springdynamodbdocker/controller/BookController.java
@@ -4,6 +4,7 @@ import com.example.springdynamodbdocker.model.Book;
 import com.example.springdynamodbdocker.service.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,5 +34,10 @@ public class BookController {
     @PostMapping
     public ResponseEntity<Book> addBook(@RequestBody Book book) {
         return ResponseEntity.ok(bookService.saveBook(book));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable("id") String id) {
+        bookService.deleteBook(id);
     }
 }
